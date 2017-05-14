0ca281551cfa4e159a65460f7bcceab66ff6e032
Tanguy PELADO <tanguy.pelado@univ-tlse3.fr>
2017-05-14 14:29:53 +0200

    améliorations + désactivation SIMPLEAUDIO
    ajouté des delays dans les lectures pour éviter une boucle trop rapide et un bordel monstre sur la Pi
    
    Viré simple audio car la lib déconne à l'install et avec les sons, je vais tester Pyglet et si ça marche bien on passera sur cette lib

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 #Author : Tanguy PELADO
-#Help : Daniel Sanchez, Aloïs Lacassagne
+#Help : Daniel Sanchez, Alois Lacassagne
 
 
 ##ToDo List
@@ -13,15 +13,15 @@
 ##Import 
 
 import time #pratique pour les delais entre les lectures
-import simpleaudio #pour lire les fichier *.wav
+#import simpleaudio #pour lire les fichier *.wav
 import RPi.GPIO as GPIO #permet l'interaction avec les Pins sur la carte
 import MFRC522 #biblio pour les lecteurs NFCs
-from sons import * #ici on importe les sons 
+#from sons import * #ici on importe les sons 
 
 ##Declaration des variables
 etat = False # defini le mode lecture ou delete (mode lecture par defaut)
 gagne = False #defini si le jeu est gagne ou pas (faux par defaut)
-tour_j1 = True # fonctionne en paire avec tour_j2, est utilisé pour changer de personne
+tour_j1 = True # fonctionne en paire avec tour_j2, est utilise pour changer de personne
 tour_j2 = False
 #j = 0 # je sais pas a quoi celui ci sert, si ca plante c'est que c'etait utile
 
@@ -29,7 +29,7 @@ tour_j2 = False
 GPIO.setmode(GPIO.BOARD) #mode BCM non dispo a cause de MFRC
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP) #declaration du bouton de tour    
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#declaration du bouton changement de mode (lecture // delete)
-#insérer ici le bouton reset
+#inserer ici le bouton reset
 GPIO.setup(40, GPIO.OUT)
 GPIO.output(40,GPIO.LOW)
 
@@ -53,7 +53,7 @@ class carte: # la classe carte, la base du jeu
 
 #12 cartes pour chaque joueurs dans 2 liste respectives  j1 et j2
 def initialisation():#initialise la liste de carte et revoie le nombre de carte par joeur ainsi que les listes de cartes
-    noms = ["Benoit","Jean-Luc","Manuel","Francois","Donald","Jacques","Marine","Nathalie","Segolene","Martine","Bernadette","Elisabeth"] #noms des cartes, en supposant que les 2 joueurs on les memes noms de cartes
+    noms = ["Delphine","Anna","Joseline","Alice","Véro","Cathy","Olivier","jacques","Mathieu","Daniel","Enrique","Henri"] #noms des cartes, en supposant que les 2 joueurs on les memes noms de cartes
     uids_j1 = [[136,4,123,218],[136,4,123,211],[136,4,122,208],[136,4,122,98],[136,4,122,104
 ],[136,4,122,110],[136,4,122,117],[136,4,122,124],[136,4,125,254],[136,4,125,1],[136,4,126,6],[136,4,124,4]] #liste des uids pour les cartes du j1
     uids_j2 = [[136,4,123,254],[136,4,123,215],[136,4,122,204],[136,4,122,101],[136,4,122,107],[136,4,122,113],[136,4,122,120],[136,4,122,127],[136,4,124,251],[136,4,126,3],[136,4,124,8],[136,4,124,0]] #liste des uids pour les cartes du j2
@@ -141,6 +141,8 @@ select_j1, select_j2 = selection() # selection des cartes a faire deviner
 ##MG Main
 #sachant que le tour du j2 est exactement le meme que celui du j1, le tour du j2
 while(not gagne): # tant que le jeu n'est pas fini
+    if(etat):#si jamais on etait en mode delete, on repasse en mode lecture
+        relais(12)
     print("tour j1")#pour le debug
     while(tour_j1 and not condi_victoire_j2(j2)): #tant que je tour n'est pas fini. on regarde aussi si le j2 n'a pas gagne
         if(etat): ## mode delete -> bouton pour supr les cartes
@@ -157,6 +159,8 @@ while(not gagne): # tant que le jeu n'est pas fini
                             print("delete")#pour le debug
                             print(j1[i].nom)#pour le debug
                         time.sleep(2)# on pause 2 secondes afin de ne pas supprimer et remettre la meme carte 1545 fois en une lecture
+            else:
+                time.sleep(0.5)
         else: #mode lecture : on lit la carte et on joue son nom
             lect = lecture()
             if(lect != None):
@@ -165,6 +169,8 @@ while(not gagne): # tant que le jeu n'est pas fini
                         print("lecture")
                         print(j1[i].nom)
                         time.sleep(1)#pour eviter de lire 556684 fois la meme carte
+            else:
+                time.sleep(0.5)
     #condition de victoire
     print("tour j2")#pour le debug
     if(etat):#si jamais on etait en mode delete, on repasse en mode lecture
@@ -185,6 +191,8 @@ while(not gagne): # tant que le jeu n'est pas fini
                                 print("delete")#pour le debug
                                 print(j2[i].nom)#pour le debug
                             time.sleep(2)
+                else:
+                    time.sleep(0.5)
             else: #mode lecture
                 lect = lecture()
                 if(lect != None):
@@ -193,6 +201,8 @@ while(not gagne): # tant que le jeu n'est pas fini
                             print("lecture")#pour le debug
                             print(j2[i].nom)#pour le debug
                             time.sleep(1)
+                else:
+                    time.sleep(0.5)
         if(condi_victoire_j2(j2)):
             print("j2 gagne")#pour le debug
         
